9ffb20241b4075e41eda770f0959c36ef2626894
Moustafa Ahmed <mous.ahmed@outlook.com>
2019-06-29 08:38:09 +0200

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var elixir = require('laravel-elixir');
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
- |--------------------------------------------------------------------------
+ |---------------------------------------p-----------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
  | for your Laravel application. By default, we are compiling the Sass
@@ -11,6 +11,21 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+elixir(function (mix) {
+    mix.sass('app.scss')
+        .styles([
+            'libs/blog-post.css',
+            'libs/bootstrap.css',
+            'libs/font-awesome.css',
+            'libs/metisMenu.css',
+            'libs/sb-admin-2.css',
+            'libs/styles.css'
+        ], './public/css/libs.css')
+        .scripts([
+            'libs/jquery.js',
+            'libs/bootstrap.js',
+            'libs/metisMenu.js',
+            'libs/sb-admin-2.js',
+            'libs/scripts.js'
+        ], './public/js/libs.js')
 });
