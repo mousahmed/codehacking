7e642a5545a1c45576c12fa1d5ddf954cf2d8adc
Moustafa Ahmed <mous.ahmed@outlook.com>
2019-07-17 16:19:19 +0200

    Added disqus comment package to posts for commenting and replying to posts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -20,7 +20,8 @@
     <hr>
 
     <!-- Preview Image -->
-    <img height="400px" width="600px" class="img-responsive" src="{{$record->photo ? $record->photo->path :  $record->photoPlaceHolder()}}" alt="">
+    <img height="400px" width="600px" class="img-responsive"
+         src="{{$record->photo ? $record->photo->path :  $record->photoPlaceHolder()}}" alt="">
 
     <hr>
 
@@ -28,109 +29,28 @@
     <p>{!! $record->content !!}</p>
 
     <hr>
-    @if(Auth::check())
-        @if(Session::has('comment_message'))
-            <div class="alert alert-success"> {{session('comment_message')}}</div>
-        @endif
-        @if(Session::has('reply_message'))
-            <div class="alert alert-success"> {{session('reply_message')}}</div>
-        @endif
-        <div class="well">
-            {!! Form::open(['method'=>'POST', 'action'=>'PostCommentsController@store',]) !!}
-            {{ csrf_field() }}
-            <input type="hidden" name="post_id" value={{$record->id}}>
-            <div class="form-group">
-                <h4>
-                    {!! Form::label('content','Leave a Comment:', ['class' =>'control-label']) !!}
-                </h4>
-                {!! Form::textarea('content',null,['class'=>'form-control','rows'=>4]) !!}
+    <div id="disqus_thread"></div>
+    <script>
 
-            </div>
-            <div class="form-group">
-                {!! Form::submit('Submit',['class'=>'btn btn-primary']) !!}
-            </div>
-
-            {!! Form::close() !!}
-        </div>
-
-        <hr>
-    @endif
-    <!-- Posted Comments -->
+        /**
+         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+        /*
+        var disqus_config = function () {
+        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+        */
+        (function () { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://codehacking-rvljm81qeh.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
 
 
-    <!-- Comment -->
-    @if($comments)
-
-        @foreach($comments as $comment)
-            @if($comment->is_active == 1)
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="{{Auth::user()->gravatar}}" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">{{$comment->author}}
-                            <small>{{$comment->created_at->format('j F Y h:i A')}}</small>
-                        </h4>
-                    {{$comment->content}}
-                    <!-- Nested Comment -->
-
-                        @if(count($comment->replies) > 0 )
-                            @foreach($comment->replies as $reply)
-                                <div class="nested-comment media" style="margin-top: 40px;">
-                                    @if($reply->is_active == 1)
-
-                                        <a class="pull-left" href="#">
-                                            <img class="media-object"
-                                                 src="{{Auth::user()->gravatar}}" alt="">
-                                        </a>
-                                        <div class="media-body">
-                                            <h4 class="media-heading">{{$reply->author}}
-                                                <small>{{$reply->created_at->format('j F Y h:i A')}}</small>
-                                            </h4>
-                                            {{$reply->content}}
-                                        </div>
-
-                                    @endif
-                                </div>
-                            @endforeach
-                        @endif
-
-                        <div class="comment-reply-container" style="margin-top: 20px">
-                            <button class="toggle-reply btn btn-primary pull-right">Reply</button>
-                            @if(Auth::check())
-                                <div class="comment-reply col-sm-10 " style="margin-top: 20px">
-                                    <div class="well">
-                                        {!! Form::open(['method'=>'POST', 'action'=>'CommentRepliesController@createReply',]) !!}
-                                        {{ csrf_field() }}
-                                        <input type="hidden" name="comment_id" value={{$comment->id}}>
-                                        <div class="form-group">
-                                            <h4>
-                                                {!! Form::label('content','Leave a Reply:', ['class' =>'control-label']) !!}
-                                            </h4>
-                                            {!! Form::textarea('content',null,['class'=>'form-control','rows'=>2]) !!}
-
-                                        </div>
-                                        <div class="form-group">
-                                            {!! Form::submit('Submit',['class'=>'btn btn-primary']) !!}
-                                        </div>
-
-                                        {!! Form::close() !!}
-
-                                    </div>
-                                </div>
-                        @endif
-
-
-                        <!-- End Nested Comment -->
-                        </div>
-
-                    </div>
-                </div>
-            @endif
-            <hr>
-        @endforeach
-    @endif
-
+    <script id="dsq-count-scr" src="//codehacking-rvljm81qeh.disqus.com/count.js" async></script>
 @endsection
 
 @section('scripts')
